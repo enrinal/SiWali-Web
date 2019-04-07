02310b144bd02d21b04685ee7e573864fdc17b1c
Alvijar <alvijarp11@gmail.com>
2019-04-07 17:02:05 +0700

    Create input_dosen.php

---
+++ application/views/admin/input_dosen.php
@@ -0,0 +1,82 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>SiWali</title>
+
+  <!-- Custom fonts for this template-->
+  <link href="<?php echo base_url('assets/vendor/fontawesome-free/css/all.css');?>" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="<?php echo base_url('assets/css/sb-admin-2.css');?>" rel="stylesheet">
+
+</head>
+<body class="bg-gradient-primary">
+
+  <div class="container">
+
+    <div class="card o-hidden border-0 shadow-lg my-5">
+      <div class="card-body p-0">
+        <!-- Nested Row within Card Body -->
+          <div class="col-xl-auto">
+            <div class="p-5">
+              <div class="text-center">
+                <h1 class="h4 text-gray-900 mb-4">Tambah Dosen!</h1>
+              </div>
+              <form action="<?php echo base_url(). 'admin/tambah_dosen'; ?>" method="post">
+                <div class="form-group">
+                  <input type="name" class="form-control" name="nama" placeholder="Name">
+                </div>
+                <div class="form-group">
+                  <input type="number" class="form-control" name="nip" placeholder="NIP DOSEN">
+                </div>
+                  <!-- <div class="form-group">
+                    <input type="text" class="form-control" name="username" placeholder="Username" readonly>
+                  </div>
+                  <div class="form-group">
+                    <input type="text" class="form-control" name="password" placeholder="Password" readonly>
+                  </div> -->
+                  <input class="btn btn-primary btn-user btn-block" type="submit" value="Input">
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+
+
+  <!-- Bootstrap core JavaScript-->
+  <script src="<?php echo base_url('assets/vendor/jquery/jquery.min.js');?>"></script>
+  <script src="<?php echo base_url('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')?>"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="<?php echo base_url('assets/vendor/jquery-easing/jquery.easing.min.js');?>"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="<?php echo base_url('assets/js/sb-admin-2.min.js');?>"></script>
+
+  <!-- Page level plugins -->
+  <script src="<?php echo base_url('assets/vendor/datatables/jquery.dataTables.min.js');?>"></script>
+
+  <!-- Page level custom scripts -->
+  <script src="<?php echo base_url('assets/vendor/datatables/dataTables.bootstrap4.min.js');?>"></script>
+  <script src="<?php echo base_url('assets/js/demo/datatables-demo.js');?>"></script>
+
+
+</body>
+
+</html>
