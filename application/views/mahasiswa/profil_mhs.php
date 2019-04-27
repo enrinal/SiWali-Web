9c6e0a916e601ce099cb14fcc83c2423cae4c484
Ayu Rahmawati <ayurahmawatii@users.noreply.github.com>
2019-04-27 10:08:44 +0700

    update profil mahasiswa
    merapihkan profil mahasiswa

--- application/views/mahasiswa/profil_mhs.php
+++ application/views/mahasiswa/profil_mhs.php
@@ -9,82 +9,149 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <!------ Include the above in your HEAD tag ---------->
 
 <!DOCTYPE html>
-
+<html>
 <head>
-	
-	<title>Profil</title>
-	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha256-3dkvEK0WLHRJ7/Csr0BZjAWxERc5WH7bdeUya2aXxdU= sha512-+L4yy6FRcDGbXJ9mPG8MT/3UCDzwR9gPeyFNMCtInsol++5m3bk2bXWKdZjvybmohrAsn3Ua5x8gfLnbE1YkOg==" crossorigin="anonymous">
-<!-- Custom CSS -->
+	<meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Profil</title>
 
-<style type="text/css">
-	body {
+  <!-- Bootstrap -->
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/bootstrap.min.css');?>">
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/prfl_mhs.css');?>">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 
-		padding-top: 20px;
-	}
-	.othertop {margin-top: 10px}
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="<?php echo base_url('assets/js/bootstrap.min.js')?>"></script>
 
-	.container-content{
-    background: #fff;
-    box-shadow: 4px 4px 22px 0px rgba(0, 0, 0, 0.2);
-    padding-top: 20px;
-    margin-right: 0px;
-    margin-right: auto;
-    margin-left: auto;
-    padding-left: 15px;
-    padding-right: 15px;
-}
-
-	#kiri
-	{
-	width:30%;
-	height:100%;
-	float:left;
-	padding-left: 70px;
-	}
-
-	#kanan
-	{
-	width:60%;
-	height:100%;
-	float:right;
-	}
-	#left
-	{
-		padding-left: 70px;
-	}
-	
-	
-</style>
 </head>
-
 <body>
-		<?php $this->load->view('Mahasiswa/topbar_mahasiswa'); ?>
+	<?php $this->load->view('Mahasiswa/topbar_mahasiswa'); ?>
 		<div class="container-content">
 		<?php $this->load->view('Mahasiswa/sidebar_p-mhs'); ?>
-	<div class="container">
-		<div class="colomn">
-			<div class="col-md-8">
-				<div class="form-horizontal">
+		<div class="row">
+  		<div class="col-md-offset-4">
+  			<div class="col-md-11">
+  				<h3>Profil Mahasiswa</h3><hr>
+  			</div>
+  			<form>
+  				<div class="col-sm-11 form-group">
+  					<div class="col-sm-4">
+  						<img src="<?php echo base_url('assets/css/image/avatar.png')?>" class="avatar img-circle img-thumbnail">
+  						<br> <br>
 
-					<div id="left">
-					<div class="left">
-						<h3>Profile Mahasiswa</h3>
-						<hr>
+  						<input class="up-file" type="file">
+  					</div>
+
+  					<label class="col-sm-2 text-left">Nama</label>
+  					<label class="col-sm-1 text-right"></label>
+  					<div>
+  						<div class="input-group">
+  							<div class="input-group-addon">
+  								<i class="fa fa-user"> </i>
+  							</div>
+  							<input type="text" class="form-control" placeholder="Nama" disabled>
+  						</div>
+  					</div><br>
+
+  					<label class="col-sm-2 text-left">NIM</label>
+  					<label class="col-sm-1 text-right"></label>
+  					<div>
+  						<div class="input-group">
+  							<div class="input-group-addon">
+  								<i class="fa fa-tag"> </i>
+  							</div>
+  							<input type="text" class="form-control" placeholder="NIM" disabled>
+  						</div>
+  					</div><br>
+
+  					<label class="col-sm-2 text-left">Angkatan</label>
+  					<label class="col-sm-1 text-right"></label>
+  					<div>
+  						<div class="input-group">
+  							<div class="input-group-addon">
+  								<i class="fa fa-list"> </i>
+  							</div>
+  							<input type="text" class="form-control" placeholder="Angkatan" disabled>
+  						</div>
+  					</div><br>
+
+  					<label class="col-sm-2 col-sm-offset-4 text-left">Dosen Wali</label>
+  					<label class="col-sm-1 text-right"></label>
+  					<div>
+  						<div class="input-group">
+  							<div class="input-group-addon">
+  								<i class="fa fa-female"> </i>
+  							</div>
+  							<input type="text" class="form-control" placeholder="Dosen Wali" disabled>
+  						</div>
+  					</div><br>
+
+  					<div class="form-group">
+					<label class="col-sm-2 col-sm-offset-4 text-left">Tempat Lahir</label>
+  					<label class="col-sm-1 text-right"></label>
+						<div class="input-group">
+  							<div class="input-group-addon">
+  								<i class="fa fa-home"> </i>
+  							</div>
+  							<input type="text" class="form-control" placeholder="Tempat Lahir">
+  						</div>
+  					</div>
+
+  					<div class="form-group">
+  						<label class="col-sm-2 col-sm-offset-4 text-left">Tanggal Lahir</label>
+  						<label class="col-sm-1 text-right"></label>
+  						<div class="input-group">
+							<div class="input-group-addon">
+								<i class="fa fa-birthday-cake"></i>
+							</div>
+							<input id="Tanggal  Lahir" name="Tanggal Lahir" type="text" placeholder="Tanggal Lahir" class="form-control input-md">
 						</div>
-
 					</div>
 
-					<div id="kiri">
-						<div class="col-md-offset-3">
-							<img src="http://ssl.gstatic.com/accounts/ui/avatar_2x.png" class="avatar img-circle img-thumbnail" alt="avatar">
-					</div>
-					<div>
-						<br>
-						<input type="file" class="text-center left-block file-upload">
-					</div>						
+					<!-- Multiple Radios (inline) -->
+					<div class="form-group">
+  						<label class="col-sm-3 col-sm-offset-4 text-left">Jenis Kelamin</label>
+  						<label class="col-sm-0 text-right"></label>
+							<label class="radio-inline" for="Gender-0">
+							<input type="radio" name="Jenis Kelamin" id="Gender-0" value="1" checked="checked">
+							Laki-Laki </label>
+
+							<label class="radio-inline" for="Gender-1">
+							<input type="radio" name="Jenis Kelamin" id="Gender-1" value="2">
+							Perempuan </label>
+					</div> <br>
+
+					<div class="form-group">
+						<label class="col-sm-3 col-sm-offset-4 text-left">Nomor Telepon</label>
+  						<label class="col-sm-0 text-right"></label>
+  						<div class="input-group">
+							<div class="input-group-addon">
+								<i class="fa fa-phone"></i>
+							</div>
+							<input id="telepon" name="telepon" type="text" placeholder="Nomor telepon" class="form-control"onkeyup="checkNumber()" minlength="11" data-fv-stringlength-message="Telepon minimal 11 Karakter">
+					</div> <br>
+
+					<div class="form-group">
+						<label class="col-sm-3 col-sm-offset-4 text-left">Email</label>
+  						<label class="col-sm-0 text-right"></label>
+  						<div class="input-group">
+							<div class="input-group-addon">
+								<i class="fa fa-envelope"></i>
+							</div>
+							 <input type="text" name="emailUser" id="emailUser" onchange="ValidateEmail()" placeholder="example@mail.com" class="form-control">
+							 </div> <br>
 					</div>
 
-					<form>
+					<div class="col-sm-offset-11">
+						<a href="#" class="btn btn-success"><span class="glyphicon glyphicon-thumbs-up"></span> Simpan</a>
+				</div>
+			</div>
+			</form>
+		</div>
+	</div>
+</body>
+<!--
 						<div id="kanan">
 							<div class="colomn">
 								<div class="form-group">
@@ -161,7 +228,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 									</div>
 								</div>
 
-							<!-- Multiple Radios (inline) -->
+							<!-- Multiple Radios (inline) --><!--
 								<div class="form-group">
 									<label class="col-md-4 control-label" for="Jenis Kelamin">Jenis Kelamin</label>
 									<div class="col-md-8"> 
@@ -176,7 +243,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 									</div>
 								</div>
 
-							<!-- Text input-->
+							<!-- Text input--> <!--
 								<div class="form-group">
 									<label class="col-md-4 control-label" for="Wali">Nama Wali</label>  
 									<div class="col-md-8">
@@ -189,8 +256,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 									</div>
 								</div>
 
-							<!-- Text input-->
-								<div class="form-group">
+							<!-- Text input--> <!--
+								<div class="form-group"> 
 									<label class="col-md-4 control-label" for="Nomor telepon">Nomor Telepon </label>  
 									<div class="col-md-8">
 										<div class="input-group">
@@ -203,7 +270,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 									</div>
 								</div>
 
-							<!-- Text input-->
+							<!-- Text input--><!--
 								<div class="form-group">
 									<label class="col-md-4 control-label" for="Email">Email</label>  
 									<div class="col-md-8">
@@ -222,13 +289,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 										<a href="#" class="btn btn-success"><span class="glyphicon glyphicon-thumbs-up"></span> Simpan</a>
 									</div>
 								</div>
-
-
-
-
-
-
-
 							</div>
 						</div>
 					</form>
@@ -237,10 +297,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			</div>
 		</div>
 	</div>
-	<!-- jQuery Version 1.11.1 -->
+	<!-- jQuery Version 1.11.1 --><!--
 <script src="js/jquery.js"></script>
 
-<!-- Bootstrap Core JavaScript -->
+<!-- Bootstrap Core JavaScript --><!--
 <script src="js/bootstrap.min.js"></script>
 
 </body>
